7bd2ed6fea66d5692384cf947c511ece6fbb6fc3
Christopher Baines <cbaines8@gmail.com>
2013-09-13 16:33:53 +0100

    Add mutiple layers

--- htdocs/map/resources/map.js
+++ htdocs/map/resources/map.js
@@ -2,46 +2,51 @@ function initMap(self) {
 
     var mapnikTileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-	self.osmTileLayer = L.tileLayer(mapnikTileUrl, {
-		maxZoom: 18,
-		attribution: ""
-	});
+    self.osmTileLayer = L.tileLayer(mapnikTileUrl, {
+        maxZoom: 18,
+        attribution: ""
+    });
 
     var baseMaps = {
         "Standard": self.osmTileLayer
     };
 
-	// Create the map
-	var map = L.map( 'map', {
-		zoom: 12,
-		layers: [self.osmTileLayer]
-	}).setView([50.9354, -1.3964], 17);
+    // Create the map
+    var map = L.map( 'map', {
+        zoom: 12,
+        layers: [self.osmTileLayer]
+    }).setView([50.9354, -1.3964], 17);
 
     L.control.scale().addTo(map);
 
     var emptyFeatureCollection = { type: "FeatureCollection", features: [] };
 
-        self.layer = L.geoJson(emptyFeatureCollection, {
+    self.keywords = ['trees', 'water']
+
+    $.each(keywords, function(index, keyword) {
+        self.layers[keyword] = L.geoJson(emptyFeatureCollection, {
             style: {color: "#0000ff"},
             onEachFeature: function(feature, layer) {
             }
-        });
+        }).addTo(map);
+    });
 
-        self.layer.addTo(map);
+    L.control.layers(baseMaps, self.layers).addTo(map);
 
-    L.control.layers(baseMaps, {}).addTo(map);
-
-	return map;
+    return map;
 }
 
 function updateMap(self) {
 
     var bb = self.map.getBounds().toBBoxString();
 
-    $.post('/places/areas/trees.json', bb, function(data) {
-            self.layer.clearLayers();
-            self.layer.addData(data);
-    }, "json");
+    $.each(keywords, function(index, keyword) {
+        $.post('/places/areas/' + keyword + '.json', bb, function(data) {
+                self.layers[keyword].clearLayers();
+                self.layers[keyword].addData(data);
+        }, "json");
+    });
+
 
 }
 
