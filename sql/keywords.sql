584d425c9cbe148bcfd8c265a68dfc8cc84597c7
ads04r <ads04r@ecs.soton.ac.uk>
2013-09-16 13:28:47 +0100

    Added some new classifications.

--- sql/keywords.sql
+++ sql/keywords.sql
@@ -1,6 +1,12 @@
 drop table fluff_keywords;
 
 create table fluff_keywords as 
+select osm_id, 'pub' as keyword from planet_osm_polygon where amenity in ('pub')
+union 
+select osm_id, 'placeofworship' as keyword from planet_osm_polygon where amenity in ('place_of_worship')
+union 
+select osm_id, 'cemetary' as keyword from planet_osm_polygon where landuse in ('cemetery') or amenity in ('grave_yard')
+union 
 select osm_id, 'indoors' as keyword from planet_osm_polygon where building is not null 
 union 
 select osm_id, 'urban' as keyword from planet_osm_polygon where landuse in ('residential','retail') 
